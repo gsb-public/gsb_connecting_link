01657ee812c1d971e01113ee25d8fd11f4fa1592
Tim Plunkett <git@plnktt.com>
2014-02-18 15:45:49 -0800

    Clean up GSBConnectingLink

--- lib/GSBConnectingLink.php
+++ lib/GSBConnectingLink.php
@@ -11,31 +11,43 @@
 class GSBConnectingLink {
 
   /**
+   * The name of the link.
+   *
    * @var string
    */
   public $name;
 
   /**
+   * The unique alias for the link.
+   *
    * @var string
    */
   public $alias;
 
   /**
+   * The URL destination.
+   *
    * @var
    */
   public $destination;
 
   /**
+   * The link sponsor.
+   *
    * @var string
    */
   public $sponsor;
 
   /**
+   * The link type.
+   *
    * @var string
    */
   public $type;
 
   /**
+   * Returns the associative array of type options.
+   *
    * @return array
    */
   public function getTypeOptions() {
@@ -47,6 +59,8 @@ class GSBConnectingLink {
   }
 
   /**
+   * Indicates if this link is new.
+   *
    * @return bool
    */
   public function isNew() {
@@ -54,6 +68,8 @@ class GSBConnectingLink {
   }
 
   /**
+   * Returns the human-readable type of link.
+   *
    * @return string
    */
   public function getType() {
@@ -71,39 +87,49 @@ class GSBConnectingLink {
   }
 
   /**
+   * Returns the sponsor for the link, with a fallback.
+   *
    * @return string
    */
   public function getSponsor() {
-    $default = variable_get('gsb_connecting_link_sponsor_default', t('Graduate School of Business Library'));
-    return $this->sponsor ?: $default;
+    return $this->sponsor ?: variable_get('gsb_connecting_link_sponsor_default', t('Graduate School of Business Library'));
   }
 
   /**
+   * Loads a given link by its alias.
+   *
    * @param string $alias
    *
-   * @return \GSBConnectingLink|null
+   * @return \GSBConnectingLink|bool
+   *   The link object, or FALSE if none exist with that alias.
    */
   public static function loadByAlias($alias) {
-    $query = db_select('gsb_connecting_link', 'cl');
-    $query->fields('cl');
-    $query->condition('alias', $alias);
+    $query = db_select('gsb_connecting_link', 'cl')
+      ->fields('cl')
+      ->condition('alias', $alias)
+      ->execute();
 
-    $links = $query->execute()->fetchAllAssoc('alias', 'GSBConnectingLink');
-    return isset($links[$alias]) ? $links[$alias] : NULL;
+    return $query->fetchObject('GSBConnectingLink');
   }
 
   /**
+   * Loads a set of links based on the current pager.
+   *
    * @param array $header
+   *   The header array for the paged table.
    *
    * @return \GSBConnectingLink[]
    */
   public static function loadFromPager($header) {
-    $query = db_select('gsb_connecting_link', 'cl')->extend('TableSort')->extend('PagerDefault');
-    $query->fields('cl');
-    $query->limit(50);
-    $query->orderByHeader($header);
+    $query = db_select('gsb_connecting_link', 'cl')
+      ->extend('TableSort')
+      ->extend('PagerDefault')
+      ->fields('cl')
+      ->limit(50)
+      ->orderByHeader($header)
+      ->execute();
 
-    return $query->execute()->fetchAllAssoc('alias', 'GSBConnectingLink');
+    return $query->fetchAllAssoc('alias', 'GSBConnectingLink');
   }
 
 }
